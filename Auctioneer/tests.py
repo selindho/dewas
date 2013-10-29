1c350c06f5c936f4e872f10774345159101b5412
Sebastian Lindholm <basti.lindholm@gmail.com>
2013-10-29 21:28:14 +0200

    Wrote two more tests for better coverage. (CreateAuction)

--- Auctioneer/tests.py
+++ Auctioneer/tests.py
@@ -1,10 +1,3 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 from datetime import timedelta
 from Auctioneer.models import *
@@ -50,4 +43,68 @@ class AuctionCreationTest(TestCase):
         # Check whether email was sent
         self.assertEqual(len(mail.outbox), 1)
 
+    def test_valid_no_confirm(self):
+        start = datetime.now()
+        stop = start + timedelta(days=3)
+        pre_count = Auctions.objects.count()
 
+        response = self.client.post(path='/auctioneer/auctions/create/', data={'title': 'Title',
+                                                                               'description': 'Description',
+                                                                               'start': start,
+                                                                               'stop': stop,
+                                                                               'starting_price': 100})
+        self.assertEqual(response.status_code, 200)
+
+        session = self.client.session
+        session['valid'] = 'True'
+        session['title'] = 'Title'
+        session['description'] = 'Description'
+        session['start'] = start
+        session['stop'] = stop
+        session['starting_price'] = 100
+        session.save()
+
+        response = self.client.post(path='/auctioneer/auctions/confirm/', data={'status': 'False'})
+
+        self.assertEqual(response.status_code, 200)
+
+        post_count = Auctions.objects.count()
+
+        # Check whether auction was created
+        self.assertEqual(pre_count, post_count)
+
+        # Check whether email was sent
+        self.assertEqual(len(mail.outbox), 0)
+
+    def test_invalid_duration(self):
+        start = datetime.now()
+        stop = start + timedelta(days=2)
+        pre_count = Auctions.objects.count()
+
+        response = self.client.post(path='/auctioneer/auctions/create/', data={'title': 'Title',
+                                                                               'description': 'Description',
+                                                                               'start': start,
+                                                                               'stop': stop,
+                                                                               'starting_price': 100})
+        self.assertEqual(response.status_code, 200)
+
+        session = self.client.session
+        session['valid'] = 'False'
+        session['title'] = 'Title'
+        session['description'] = 'Description'
+        session['start'] = start
+        session['stop'] = stop
+        session['starting_price'] = 100
+        session.save()
+
+        response = self.client.post(path='/auctioneer/auctions/confirm/', data={'status': 'True'})
+
+        self.assertEqual(response.status_code, 200)
+
+        post_count = Auctions.objects.count()
+
+        # Check whether auction was created
+        self.assertEqual(pre_count, post_count)
+
+        # Check whether email was sent
+        self.assertEqual(len(mail.outbox), 0)
